de5e9a910beebd899fb0bdee85148f67b54b588f
Kathryn Hodge <kahodge@vassar.edu>
2015-04-14 12:55:24 -0400

    Mapping out the task

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tictactoe;
 
-/**
- *
- * @author kathrynhodge
- */
 public class TicTacToe {
+    // What do we need to make Tic Tac Toe:
+    
+    // Game Logic --> How do we win; how do we lose
+    // VIA gameOver method that checks things
+    
+    // Controls --> How do we play? How do we enter X's and O's
+    // VIA terminal/entering in the console
+    
+    // Monte Carlo AI -> How does the AI know what to do next?
+    // VIA probabilities + monte carlo method of running many and many 
+    // scenarios, observing the results, and then choosing where to go
+    
+    // What classes will we need?
+    // - A class managing the AI components -> how we run all the 
+    //   simulations, observe results, etc. (USING Heuristics & Probabilities)
+    // - A class that contains the logic of the game 
+    //   (onTick/update, react, display)
+    // - A custom class (or some type of data structure) that stores the current
+    //   state of the game
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // The game runs here....
     }
     
 }
