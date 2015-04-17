0150490c712fd889880680f38b9e6567ccf4099c
Kathryn Hodge <kahodge@vassar.edu>
2015-04-17 17:09:21 -0400

    Making the Game

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -43,71 +43,94 @@ package tictactoe;
 public class TicTacToe {
 
     final private char[][] board;
-    private char marker;
+    private char userMarker;
+    private char aiMarker;
+    private char currentMarker;
 
-    public TicTacToe(char userMarker) {
+
+    public TicTacToe(char userMarker, char aiMarker) {
         board = new char[3][3];
         // board[0].length for length of row
         // board.length for length of col
         for (int i = 0; i < board[0].length; i++) {
-            for (int j = 0; i < board.length; j++) {
+            for (int j = 0; j < board.length; j++) {
                 board[i][j] = '-';
             }
         }
-        marker = userMarker;
+        this.userMarker = userMarker;
+        this.aiMarker = aiMarker;
+        this.currentMarker = userMarker;
+        printBoard();
+    }
+    
+    // TRUE = user
+    // FALSE = AI
+    
+    public boolean whoseTurn() {
+        return currentMarker == userMarker;
     }
 
     public String playTurn(int row, int col) {
         // check if spot is in range
         if (row >= 0 && row < board.length && col >= 0 && col < board.length) {
             // check if spot isn't taken
-            if (!(this.isSpotTaken(row, col))) {
-                board[row][col] = marker;
+            if (this.isSpotNotTaken(row, col)) {
+                this.changeMarker();
+                board[row][col] = currentMarker;
                 return "played turn";
             }
         }
         return "invalid input";
     }
+   
     
-    public boolean isSpotTaken(int row, int col) {
+    public boolean isSpotNotTaken(int row, int col) {
         return board[row][col] == '-';
     }
 
     public void changeMarker() {
-        if (marker == 'X') {
-            marker = 'O';
+        if (currentMarker == userMarker) {
+            currentMarker = aiMarker;
         } else {
-            marker = 'X';
+            currentMarker = userMarker;
         }
     }
 
     public void printBoard() {
-        String[] boardArray = new String[3];
         // Attempting to create...
         // | - | - | -
         // ------------
         // | - | - | -
         // ------------
         // | - | - | -
+        String[] rows = new String[3];
         for (int i = 0; i < board[0].length; i++) {
-            for (int j = 0; i < board.length; j++) {
-                boardArray[i] = boardArray[i] + " | " + boardArray[j];
+            for (int j = 0; j < board.length; j++) {
+                if (rows[i] == null) {
+                    rows[i] = "";
+                }
+                rows[i] = rows[i] + " | " + board[j][i];
             }
-            System.out.println(boardArray[i]);
         }
+        for (String s : rows) {
+            System.out.println(s);
+            System.out.println("--------------");
+        }
+        System.out.println();
+        
     }
 
     public boolean isGameOver() {
         // winning conditions
         // create bools for all
-        boolean topRow = board[0][0] == board[0][1] && board[0][1] == board[0][2];
-        boolean middleRow = board[1][0] == board[1][1] && board[1][1] == board[1][2];
-        boolean bottomRow = board[2][0] == board[2][1] && board[2][1] == board[2][2];
-        boolean firstCol = board[0][0] == board[1][0] && board[1][0] == board[2][0];
-        boolean secondCol = board[0][1] == board[1][1] && board[1][1] == board[2][1];
-        boolean thirdCol = board[0][2] == board[1][2] && board[1][2] == board[2][2];
-        boolean rightDi = board[0][0] == board[1][1] && board[1][1] == board[2][2];
-        boolean leftDi = board[0][2] == board[1][1] && board [1][1] == board[2][0];
+        boolean topRow = board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][1] != '-';
+        boolean middleRow = board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][1] != '-';
+        boolean bottomRow = board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][1] != '-';
+        boolean firstCol = board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[1][0] != '-';
+        boolean secondCol = board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[1][1] != '-';
+        boolean thirdCol = board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[1][2] != '-';
+        boolean rightDi = board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != '-';
+        boolean leftDi = board[0][2] == board[1][1] && board [1][1] == board[2][0] && board[1][1] != '-';
         boolean checkConditions = topRow || middleRow || bottomRow || firstCol || secondCol || thirdCol || rightDi || leftDi;
         // ALSO NEED TO CHECK IF what we match is actually a winner -> not just '-'
         return checkConditions;
@@ -116,6 +139,8 @@ public class TicTacToe {
 
     public static void main(String[] args) {
         // The game runs here....
+        TicTacToe newGame = new TicTacToe('X', 'O');
+        newGame.printBoard();
     }
 
 }
